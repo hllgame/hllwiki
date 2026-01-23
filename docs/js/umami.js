799e143b21f1b3f4f983d6c9db23859f22d781bf
hllgame <219201811+hllgame@users.noreply.github.com>
2026-01-23 13:17:57 -0700

    umami alternate method

--- docs/js/umami.js
+++
@@ -1,9 +0,0 @@
-// umami.js
-document.addEventListener("DOMContentLoaded", function() {
-    var script = document.createElement("script");
-    script.async = true;
-    script.defer = true;
-    script.setAttribute("data-website-id", "2c1f5269-76ba-499f-8afd-b5ee31a5dc1e");
-    script.src = "https://hello.hllgame.top/script.js";
-    document.body.appendChild(script);
-});
